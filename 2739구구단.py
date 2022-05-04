ea5a404fcc2b9b53e23fab772cbbd5468e851515
hyerimyun <yoonhyerim94@gmail.com>
2022-05-05 00:12:01 +0900

    기초

---
+++ 2739구구단.py
@@ -0,0 +1,10 @@
+# a = input()
+# print(a)
+# b = int(a) + 1
+# print(b)
+
+n = int(input())
+
+for i in range(1,10):
+    print('%d * %d = %d'%(n,i,n*i))
+    
---
+++ 2839설탕배달.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+
+while True:
+    if(n % 5 == 0):
+        print(n // 5)
+    elif(n % 5 <= 1 | n % 3 == 0):
+        print(n // 3)
+    else:
+     print(-1)
+    break
+
+list = [100000] - 
