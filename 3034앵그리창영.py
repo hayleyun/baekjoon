fd0922a8903814a627fbc102e49e9dffad49ff6b
hyerimyun <yoonhyerim94@gmail.com>
2022-06-03 16:48:16 +0900

    개

--- 3034앵그리창영.py
+++ 3034앵그리창영.py
@@ -2,13 +2,11 @@ import math
 
 n, w, h = list(map(int, input().split()))
 c = math.sqrt(w**2 + h**2)
-result = ''
+
 for _ in range(n):
     b = int(input())
 
     if b <= c or b <= w or b <= h:
-        result = 'DA'
+        print('DA')
     else:
-        result = 'NE'
-
-    print(result)
+        print('NE')
